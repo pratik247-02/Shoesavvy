02dbaa5fd7ff2a8162afea02168d6fb75ae97fe9
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2024-01-02 13:21:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import authroutes from './routes/authroutes.js';
 import productroutes from './routes/productroutes.js';
 import path from 'path';
 import { fileURLToPath} from 'url';
-import { dirname } from 'path';
 
 
 //config env
@@ -16,7 +15,7 @@ dotenv.config();
 
 //esmodule fix
 const __filename= fileURLToPath(import.meta.url);
-const __dirnname= path.dirname(__filename)
+const __dirname= path.dirname(__filename)
 
 //db connection
 connectDB();
