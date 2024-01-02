10b7f8b28fbe83570f4bb2ba2a9ac3db0c0d3bb9
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2024-01-02 13:49:09 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,17 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI;
+
+    if (!uri) {
+      throw new Error('MongoDB URI is not defined. Make sure the environment variable MONGODB_URI is set.');
+    }
+
+    const connection = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error in MongoDB connection: ${error.message}`);
