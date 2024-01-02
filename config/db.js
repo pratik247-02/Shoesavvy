ad5fceed2aea2e45837e994e30b186619700ea1d
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2024-01-02 13:44:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-           
-            console.log('MongoDB Connected: ${conn.connection.host}');
-
-            
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error in MongoDB connection: ${error.message}`);
-    } 
-}
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error(`Error in MongoDB connection: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
